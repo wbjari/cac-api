724f1c3937137c649a272a1302fe3348c581adfb
Erdem <38556188+Erdempp@users.noreply.github.com>
2021-06-02 23:06:38 +0200

    Updated CarService and CustomerService
    NOT YET TESTED!

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -8,22 +8,22 @@ export class CarService {
     constructor(@InjectModel(Car.name) private readonly carModel: Model<CarDocument>) { }
 
     async create(data: Car) {
-
+        return this.carModel.create(data);
     }
 
     async getAll() {
-
+        return this.carModel.find();
     }
 
     async get(id: string) {
-
+        return this.carModel.findById(id);
     }
 
     async update(id: string, changes: Partial<Car>) {
-
+        return this.carModel.findByIdAndUpdate(id, changes);
     }
 
     async remove(id: string) {
-
+        return this.carModel.remove(id);
     }
 }
--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -8,22 +8,22 @@ export class CustomerService {
     constructor(@InjectModel(Customer.name) private readonly customerModel: Model<CustomerDocument>) { }
 
     async create(data: Customer) {
-
+        return this.customerModel.create(data);
     }
 
     async getAll() {
-
+        return this.customerModel.find();
     }
 
     async get(id: string) {
-
+        return this.customerModel.findById(id);
     }
 
     async update(id: string, changes: Partial<Customer>) {
-
+        return this.customerModel.findByIdAndUpdate(id, changes)
     }
 
     async remove(id: string) {
-
+        return this.customerModel.findByIdAndRemove(id);
     }
 }
